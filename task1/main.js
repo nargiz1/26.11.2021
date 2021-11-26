95b4ddb2dad1fa4a913d489ce84229df96073528
ACER <nargizer@code.edu.az>
2021-11-26 14:41:51 +0400

    task1

---
+++ task1/main.js
@@ -0,0 +1,19 @@
+"use strict"
+while(true){
+    let num=prompt("Enter number from 1 to 4:");
+    switch(true){
+        case (num==1):
+            alert("Winter");
+            break;
+        case (num==2):
+            alert("Spring");
+            break;
+        case (num==3):
+            alert("Summer");
+            break;
+        case (num==4):
+            alert("Fall");
+            break;
+    }
+}
+
